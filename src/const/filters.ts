bc360494f835753b29503725f8e722ba19b292bb
Danni Garcia <jodadev.col@gmail.com>
2024-09-16 21:46:49 +1000

    Add Filter component and CardCountry component

---
+++ src/const/filters.ts
@@ -0,0 +1,8 @@
+export const SELECT_REGIONS = [
+	{ name: "All Regions", value: "all" },
+	{ name: "Africa", value: "africa" },
+	{ name: "Americas", value: "americas" },
+	{ name: "Asia", value: "asia" },
+	{ name: "Europe", value: "europe" },
+	{ name: "Oceania", value: "oceania" },
+]
